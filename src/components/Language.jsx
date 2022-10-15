287da8f5c782b924af8e91e1ed1983ee03201756
berkay oren <benberkayoren@gmail.com>
2022-10-15 13:26:35 +0300

    ol

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+
 const Language = ({ name, options, img }) => {
-  return <div>Language</div>;
+  const [showDesc, setShowDesc] = useState(false);
+  console.log(showDesc);
+  return (
+    <Container
+      style={{ background: "peachpuff" }}
+      className="p-4 rounded-2"
+      onClick={() => setShowDesc(!showDesc)}
+    >
+      <Container>
+        <Image src={img} width="70%"></Image>
+        <h3 className="display-6">{name}</h3>
+      </Container>
+      <ol>
+        {options.map((item) => {
+          return <li className="h5 text-start"> {item} </li>;
+        })}
+      </ol>
+    </Container>
+  );
 };
 
 export default Language;
