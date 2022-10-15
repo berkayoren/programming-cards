0027d777811c3f54ed38a9f87004e764bb94a325
berkay oren <benberkayoren@gmail.com>
2022-10-15 12:42:55 +0300

    breakppoints

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,23 @@
-import Container from "react-bootstrap/Container";
 import Language from "./Language";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { data } from "../helpers/data";
 
 const Card = () => {
   return (
     <Container className="rounded-4 mt-4" style={{ background: "#f48b29" }}>
       <h1>Language</h1>
-      {data.map((lang, index) => {
-        return <Language {...lang} key={index} />;
-        // return <Language lang={lang} />;
-      })}
+      <Row>
+        {data.map((lang, index) => {
+          return (
+            <Col sm={6} md={4} lg={3} key={index}>
+              <Language {...lang} />
+            </Col>
+          );
+        })}
+      </Row>
+
       <Language />
     </Container>
   );
